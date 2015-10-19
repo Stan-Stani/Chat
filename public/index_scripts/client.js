11974c8e613fbf9f0d4841b39423f123d5ab8e8c
cyclingLinguist <cyclinglinguist@gmail.com>
2015-10-19 12:52:50 -0500

    Finish essential refactoring and commenting in client.js, modify package.json to reflect release

--- public/index_scripts/client.js
+++ public/index_scripts/client.js
@@ -1,6 +1,9 @@
 // ! Global Variables Section !
-// Notice that I’m not specifying any URL when I call io(), since it defaults to trying to connect to the host that serves the page.
+
+
+// Establishes connection to Server (defaults to connecting to host that served page so no URL required in argument)
 var socket = io();
+
 chatForm = document.getElementById("chat-form");
 chatFormInput = document.getElementById("chat-form-input");
 
@@ -35,7 +38,7 @@ handleClientEmits();
 // ! Central Functions' Definitions Section !
 
 
-
+// On page load, reads the 'mute-status' cookie which has the mute state of chat sounds from the last session and applies that cookie to the 'toggle-mute' button and manageSound()
 function loadMuteState() {
   var button = document.getElementById('toggle-mute');
   var keyValuePairs = document.cookie.split(/; */);
@@ -53,6 +56,7 @@ function loadMuteState() {
   }
 }
 
+// On mute button click, toggles the appearance of 'toggle-mute', toggles true/false value of manageSound.muteAll, and toggles the mute-status cookie
 function handleMuteToggle(buttonId) {
 
   button = document.getElementById(buttonId);
@@ -60,39 +64,41 @@ function handleMuteToggle(buttonId) {
 
   function toggleMute(button) {
     // If manageSound has been explicitly muted, then unmute it.
+    // Eventually need to put the contained cookie creation code in a funciton as it's used twice.
     if (manageSound.muteAll) {
       manageSound.muteAll = false;
       button.innerHTML = 'Turn Sound Off';
-        // http://stackoverflow.com/questions/3818193/how-to-add-number-of-days-to-todays-date
-          var expDate = new Date();
-          var numberOfDaysToAdd = 183;
-          var cookies = document.cookie;
-          expDate.setDate(expDate.getDate() + numberOfDaysToAdd);  
-          setCookie('mute-status', 'not-muted', expDate);
-          cookiesInfo.printConsoleChange();
-        // Easy way to delete cookie
-        //  document.cookie = 'cook' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-
+      // creates/re-creates cookie, setting it to expire 183 days from its creation/re-cretion date.
+      // http://stackoverflow.com/questions/3818193/how-to-add-number-of-days-to-todays-date
+      var expDate = new Date();
+      var numberOfDaysToAdd = 183;
+      var cookies = document.cookie;
+      expDate.setDate(expDate.getDate() + numberOfDaysToAdd);  
+      setCookie('mute-status', 'not-muted', expDate);
+      cookiesInfo.printConsoleChange();
+      // Easy way to delete cookie
+      // document.cookie = 'cook' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
     /* If manageSound.muteAll has been explicitly set to false, or has not been defined yet, mute.
       (Because manageSound() only mutes if its muteAll property has been set to true, there
       could be a case where muteAll has not been defined and we would want to mute on toggle if that's true.
-      Basically if muteAll has not been defined we want to default to not muting, but mute on toggle.) */
+      Basically if muteAll has not been defined (if user hasn't ever clicked toggle button) we want to default to not muting, but mute on toggle.) */
     else {
       manageSound.muteAll = true;
       button.innerHTML = 'Turn Sound On'
-
-          var expDate = new Date();
-          var numberOfDaysToAdd = 183;
-          var cookies = document.cookie;
-          expDate.setDate(expDate.getDate() + numberOfDaysToAdd);  
-          setCookie('mute-status', 'muted', expDate);
-          cookiesInfo.printConsoleChange();
+      // See above
+      var expDate = new Date();
+      var numberOfDaysToAdd = 183;
+      var cookies = document.cookie;
+      expDate.setDate(expDate.getDate() + numberOfDaysToAdd);  
+      setCookie('mute-status', 'muted', expDate);
+      cookiesInfo.printConsoleChange();
     }
 
   }
 }
 
+// Tells the client user that their client has connected.
 function alertClientConnect() {
   alertClient('Notice: Connection established!');
   var sound = document.getElementById('yahhoo');
@@ -100,7 +106,7 @@ function alertClientConnect() {
 }
 
 
-// Does things based off of the events emitted by the server.
+// Does things based off of the events emitted by the server
 function handleServerEmits() {
   var messages = document.getElementById('messages')
   socket.on('chat message', function(msg){
@@ -110,6 +116,7 @@ function handleServerEmits() {
     manageSound(sound);
   });
   
+  // Doesn't play sound when client receives its own chat message back from the server
   socket.on('own chat message', function(msg){
     alertClient(msg);
     scrollMessagesDown();
@@ -144,6 +151,7 @@ function handleServerEmits() {
   };
 };
 
+// Emits events to server based off of things that happen in the client
 function handleClientEmits() {
   chatForm.addEventListener("submit", function(ev){
     // ev.preventDefault(); prevents form from actually submitting and thus the page from refreshing (but the event listener still fires)
@@ -157,8 +165,7 @@ function handleClientEmits() {
 
 
 // ! Reusable Component Functions' Definitions Section !
-// These are functions that are used (or will be used) in more than one Central Function (so they're defined once on the same level as the Central Functions as opposed to multiple times
-// inside of the Central Functions).
+/* These are functions that are used (or will be used) in more than one Central Function (so they're defined once on the same level as the Central Functions as opposed to multiple times inside of the Central Functions). */
 
 function setCookie(name, value, expDate) {
   //If expDate exists then convert to UTC format
@@ -171,6 +178,7 @@ function alertClient(msg) {
   messages.innerHTML += '<li>' + msg + '</li>'
 };
 
+// Consider placing inside alertClient and only use alertClient() to write to messages. alertClient() might need a rename if we do that
 function scrollMessagesDown() {
   var objDiv = document.getElementById('message-container');
   objDiv.scrollTop = objDiv.scrollHeight;
